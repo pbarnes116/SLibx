d0bda88e68fa2f771184d3fb0ac793bf66902565
f <f@f.f>
2015-03-30 19:13:37 +0800

    update

--- map/z_slib_map_mapview.cpp
+++ map/z_slib_map_mapview.cpp
@@ -147,65 +147,41 @@ sl_bool MapView::onMouseEvent(MouseEvent& event)
 		|| event.action == MouseEvent::actionTouchUp
 		) {
 
-		double dx = (event.x - m_mouseDownX) / m_viewportWidth;
-		double dy = -(event.y - m_mouseDownY) / m_viewportHeight;
-		double dt = (double)((Time::now() - m_timeMouseDown).getMillisecondsCount());
+		if (!m_flagTouch2) {
 
-		sl_real time = 500;
-		if (event.action == MouseEvent::actionLeftButtonUp
-			|| event.action == MouseEvent::actionTouchUp) {
-			if (dt < 400) {
-				time = 4000;
-				dx *= 8;
-				dy *= 8;
-			}
-			if (dt < 100 && dx * dx + dy * dy < 0.01 ) {
-				time = 0;
-			}
-		}
+			double dx = (event.x - m_mouseDownX) / m_viewportWidth;
+			double dy = -(event.y - m_mouseDownY) / m_viewportHeight;
+			double dt = (double)((Time::now() - m_timeMouseDown).getMillisecondsCount());
 
-		GeoLocation locEye = camera->getEyeLocation();
-		double alt = locEye.altitude;
-		Vector3lf posViewSurface(-dx * alt, -dy * alt, alt);
-		Vector3 pos = m_transformDown.inverse().transformPosition(posViewSurface);
-		GeoLocation locTarget = MapEarth::getGeoLocation(pos);
-		if (time > 0) {
-			camera->startMoving(GeoLocation(locTarget.getLatLon(), alt), time);
-		} else {
-			camera->stopMoving();
+			sl_real time = 500;
+			if (event.action == MouseEvent::actionLeftButtonUp
+				|| event.action == MouseEvent::actionTouchUp) {
+				if (dt < 400) {
+					time = 4000;
+					dx *= 8;
+					dy *= 8;
+				}
+				if (dt < 100 && dx * dx + dy * dy < 0.01) {
+					time = 0;
+				}
+			}
+
+			GeoLocation locEye = camera->getEyeLocation();
+			double alt = locEye.altitude;
+			Vector3lf posViewSurface(-dx * alt, -dy * alt, alt);
+			Vector3 pos = m_transformDown.inverse().transformPosition(posViewSurface);
+			GeoLocation locTarget = MapEarth::getGeoLocation(pos);
+			if (time > 0) {
+				camera->startMoving(GeoLocation(locTarget.getLatLon(), alt), time);
+			} else {
+				camera->stopMoving();
+			}
 		}
 
 		m_mouseBeforeX = event.x;
 		m_mouseBeforeY = event.y;
 		m_timeMouseBefore = Time::now();
-		/*
-		sl_real dx = event.x - m_mouseDownX;
-		sl_real dy = event.y - m_mouseDownY;
-		sl_real rx = dx / 100.0f;
-		sl_real ry = dy / 100.0f;
-		Matrix4 matInv = (m_lastEarthMatrix * m_environment->cameraViewEarth.getViewMatrix()).inverse();
-		Matrix4 mat = Transform3D::getRotationMatrix(Transform3D::getTransformedAxisX(matInv), ry) * m_lastEarthMatrix;
-		mat.multiply(Transform3D::getRotationMatrix(Vector3::axisY(), rx));
-		m_environment->setEarthTransform(mat);
-		
-		if (!m_flagTouch2) {
-			sl_real dx = event.x - m_mouseBeforeX;
-			sl_real dy = event.y - m_mouseBeforeY;
-			Sphere earth(Transform3::getTransformedOrigin(m_environment->transformView), (sl_real)(MapEarth::getRadius()));
-			Line3 dirScreenO = Transform3::unprojectScreenPoint(m_environment->transformProjection, Point(0, 0), this->getClientRectangle());
-			Line3 dirScreenX = Transform3::unprojectScreenPoint(m_environment->transformProjection, Point(1, 0), this->getClientRectangle());
-			Line3 dirScreenY = Transform3::unprojectScreenPoint(m_environment->transformProjection, Point(0, 1), this->getClientRectangle());
-			sl_real lx = (dirScreenO.getDirection().getNormalized() - dirScreenX.getDirection().getNormalized()).getLength();
-			sl_real ly = (dirScreenO.getDirection().getNormalized() - dirScreenY.getDirection().getNormalized()).getLength();
-			sl_real h = (sl_real)(m_environment->cameraViewEarth->getEyeLocation().altitude);
-			sl_real f = (sl_real)(Math::getDegreeFromRadian(h * 1.5f) / MapEarth::getRadius());
-			m_environment->cameraViewEarth->setEyeLocation(GeoLocation(
-				le.latitude + dy * ly * f
-				, le.longitude -dx * lx * f
-				, le.altitude));
-		}
-		m_mouseBeforeX = event.x;
-		m_mouseBeforeY = event.y;
+
 		if (event.points.count() == 2) {
 			sl_real dx2 = event.points[1].x - event.x;
 			sl_real dy2 = event.points[1].y - event.y;
@@ -213,15 +189,15 @@ sl_bool MapView::onMouseEvent(MouseEvent& event)
 				Vector2 v1(m_mouseBefore2X, m_mouseBefore2Y);
 				Vector2 v2(dx2, dy2);
 				if (v1.length() > 10 && v2.length() > 10) {
-					zoom(Math::square(v1.length() / v2.length()));
-				}
+					_zoom(Math::square(v1.length() / v2.length()));
+				}				
 			}
 			m_mouseBefore2X = dx2;
 			m_mouseBefore2Y = dy2;
 			m_flagTouch2 = sl_true;
 		} else {
 			m_flagTouch2 = sl_false;
-		}*/
+		}
 		requestRender();
 
 	} else if (event.action == MouseEvent::actionRightButtonDown) {
@@ -286,6 +262,7 @@ void MapView::_zoom(double ratio)
 		}
 		le.altitude = h;
 		getCamera()->setEyeLocation(le);
+		getCamera()->stopMoving();
 	}
 }
 
